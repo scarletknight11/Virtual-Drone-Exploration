e14e18c09f380fb3f5e40c1f18e2c27608afd74c
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-03-10 04:31:21 -0500

    Got some redesigns to level and full movement of drone working. Drone can also be controlled via PS4 Controller

--- Assets/Scripts/Drone_Engine.cs
+++ Assets/Scripts/Drone_Engine.cs
@@ -18,9 +18,15 @@ namespace VRLab {
 
         public void UpdateEngine(Rigidbody rb, Drone_Inputs input)
         {
+            Vector3 upVec = transform.up;
+            upVec.x = 0f;
+            upVec.z = 0f;
+            float diff = 1 - upVec.magnitude;
+            float finalDiff = Physics.gravity.magnitude * diff;
+
             Vector3 engineForce = Vector3.zero;
             //adds more hover power up or going down and we want divide by number of engines we have
-            engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude) + (input.Throttle * maxPower)) / 8f;
+            engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude + finalDiff) + (input.Throttle * maxPower)) / 8f;
 
             rb.AddForce(engineForce, ForceMode.Force);
         }
