55d7ba17ae546a0ca599f4cba4422eae0f9a4477
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-03-10 15:33:41 -0500

    can switch from FPS To third person view

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public int CamMode;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) {
+             if (CamMode == 1) {
+                 CamMode = 0;
+            } else {
+                CamMode += 1;
+            }
+            StartCoroutine(CamChange());
+        }  
+    }
+
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (CamMode == 0)
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+        if (CamMode == 1)
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+    }
+
+}
