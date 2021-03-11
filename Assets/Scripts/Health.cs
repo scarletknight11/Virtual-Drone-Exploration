cfb80a67344ef7ad141defb66acae4b3c6d178dd
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-03-10 19:54:00 -0500

    got gameplay finally working

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,75 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Health : MonoBehaviour {
-	
-	public enum deathAction {loadLevelWhenDead,doNothingWhenDead};
-	
+public class Health : MonoBehaviour
+{
+
+	public float damageAmount = 10.0f;
+
+	public float delayBeforeDestroy = 0.0f;
+
+	public enum deathAction { loadLevelWhenDead, doNothingWhenDead };
+
 	public float healthPoints = 1f;
-	public float respawnHealthPoints = 1f;		//base health points
-	
-	public int numberOfLives = 1;					//lives and variables for respawning
-	public bool isAlive = true;	
+	public float respawnHealthPoints = 1f;      //base health points
+
+	public int numberOfLives = 1;              //lives and variables for respawning
+	public bool isAlive = true;
 
 	public GameObject explosionPrefab;
-	
+
 	public deathAction onLivesGone = deathAction.doNothingWhenDead;
-	
+
 	public string LevelToLoad = "";
-	
+
 	private Vector3 respawnPosition;
 	private Quaternion respawnRotation;
-	
 
-	// Use this for initialization
-	void Start () 
+	private LifeManager lifeSystem;
+
+	//Use this for initialization
+	void Start()
 	{
-		// store initial position as respawn location
+		//LifeManager script to Health.cs
+		lifeSystem = FindObjectOfType<LifeManager>();
+
+		//store initial position as respawn location
 		respawnPosition = transform.position;
 		respawnRotation = transform.rotation;
-		
-		if (LevelToLoad=="") // default to current scene 
+
+		if (LevelToLoad == "") // default to current scene 
 		{
 			LevelToLoad = Application.loadedLevelName;
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	//Update is called once per frame
+	void Update()
 	{
-		if (healthPoints <= 0) {				// if the object is 'dead'
-			numberOfLives--;					// decrement # of lives, update lives GUI
-			
-			if (explosionPrefab!=null) {
-				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		if (healthPoints < 1)
+		{               // if the object is 'dead'
+			numberOfLives--;                    // decrement # of lives, update lives GUI
+
+			if (explosionPrefab != null)
+			{
+				Instantiate(explosionPrefab, transform.position, Quaternion.identity);
 			}
-			
-			if (numberOfLives > 0) { // respawn
-				transform.position = respawnPosition;	// reset the player to respawn position
+
+			if (numberOfLives > 0)
+			{ // respawn
+				transform.position = respawnPosition;   // reset the player to respawn position
 				transform.rotation = respawnRotation;
-				healthPoints = respawnHealthPoints;	// give the player full health again
-			} else { // here is where you do stuff once ALL lives are gone)
+				healthPoints = respawnHealthPoints; // give the player full health again
+			}
+			else
+			{ // here is where you do stuff once ALL lives are gone)
 				isAlive = false;
-				
-				switch(onLivesGone)
+
+				switch (onLivesGone)
 				{
-				case deathAction.loadLevelWhenDead:
-					Application.LoadLevel (LevelToLoad);
-					break;
-				case deathAction.doNothingWhenDead:
-					// do nothing, death must be handled in another way elsewhere
-				                                                                                                                   	break;
+					case deathAction.loadLevelWhenDead:
+						Application.LoadLevel(LevelToLoad);
+						break;
+					case deathAction.doNothingWhenDead:
+						// do nothing, death must be handled in another way elsewhere
+						break;
 				}
 				Destroy(gameObject);
 			}
 		}
 	}
-	
+
 	public void ApplyDamage(float amount)
-	{	
-		healthPoints = healthPoints - amount;	
+	{
+		healthPoints = healthPoints - amount;
+		//lifeSystem.TakeLife();
 	}
-	
+
 	public void ApplyHeal(float amount)
 	{
 		healthPoints = healthPoints + amount;
@@ -79,8 +95,9 @@ public class Health : MonoBehaviour {
 	{
 		numberOfLives = numberOfLives + amount;
 	}
-	
-	public void updateRespawn(Vector3 newRespawnPosition, Quaternion newRespawnRotation) {
+
+	public void updateRespawn(Vector3 newRespawnPosition, Quaternion newRespawnRotation)
+	{
 		respawnPosition = newRespawnPosition;
 		respawnRotation = newRespawnRotation;
 	}
---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class LifeManager : MonoBehaviour {
+
+    public int startingLives = 1;
+    public int lifeCounter;
+    
+    private Text theText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        theText = GetComponent<Text>();
+
+        lifeCounter = startingLives;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        theText.text = "x " + lifeCounter;
+	}
+
+    public void GiveLife()
+    {
+        lifeCounter++;
+    }
+
+    public void TakeLife()
+    {
+        lifeCounter--;
+
+        if(lifeCounter <= 0)
+        {
+            lifeCounter = 0;
+        }
+    }
+
+}
